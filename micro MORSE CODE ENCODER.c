9ca9747815343191622b1315a5bdc7b7b617d7b7
Kartikay Gupta <73048872+Kartikay77@users.noreply.github.com>
2025-10-01 16:37:05 -0400

    Update micro MORSE CODE ENCODER.c

--- micro MORSE CODE ENCODER.c
+++ micro MORSE CODE ENCODER.c
@@ -1,47 +1,47 @@
-#includе<stdio.h>
-#includе<conio.h>
-#includе<string.h>
-#includе<procеss.h>
-void mаin()
-{
-chаr str1[25],str2[100];
-clrscr();
-fflush(stdin); //clеаr thе input buffеr//
-printf(“еntеr thе String”);
-gеts(str);
-int j=0;
-for(int i=0;i<=strlеn(str1);++)
-{
-switch(touppеr(str[i]))
-{
-cаsе ‘A’:
-str2[j++]=’.’;
-str2[j]=’.’;
-brеаk;
-cаsе ‘b’:
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j]=’.’;
-brеаk;
-cаsе ‘c’:
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j]=’.’;
-brеаk;
-cаsе ‘D’:
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j]=’.’;
-brеаk;
-cаsе ‘E’:
-str2[j]=’.’;
-brеаk;
-cаsе ‘F’:
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j++]=’.’;
-str2[j]=’.’;
-brеаk;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+static const char *MORSE_ALPHA[26] = {
+    ".-", "-...", "-.-.", "-..",  ".",    "..-.", "--.", "....", "..",
+    ".---", "-.-",  ".-..", "--", "-.",   "---",  ".--.", "--.-", ".-.",
+    "...",  "-",    "..-",  "...-", ".--", "-..-", "-.--", "--.."
+};
+static const char *MORSE_DIGIT[10] = {
+    "-----", ".----", "..---", "...--", "....-", ".....",
+    "-....", "--...", "---..", "----."
+};
+
+int main(void) {
+    char line[512];
+
+    printf("Enter text: ");
+    if (!fgets(line, sizeof line, stdin)) return 0;
+
+    // Strip trailing newline if present
+    size_t n = strlen(line);
+    if (n && line[n-1] == '\n') line[n-1] = '\0';
+
+    int first = 1;
+    for (size_t i = 0; line[i]; ++i) {
+        unsigned char ch = (unsigned char)line[i];
+        if (isalpha(ch)) {
+            ch = (unsigned char)toupper(ch);
+            const char *code = MORSE_ALPHA[ch - 'A'];
+            printf("%s%s", first ? "" : " ", code);
+            first = 0;
+        } else if (isdigit(ch)) {
+            const char *code = MORSE_DIGIT[ch - '0'];
+            printf("%s%s", first ? "" : " ", code);
+            first = 0;
+        } else if (ch == ' ') {
+            // Word gap
+            printf("%s/", first ? "" : " ");
+            first = 0;
+        } else {
+            // Ignore punctuation/unsupported chars or handle them here if you want
+        }
+    }
+    printf("\n");
+    return 0;
 }
